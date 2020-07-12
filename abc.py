c166574d8e152f092e0b38c9dc41178af1df9644
Jaydeep Mahajan <jaydeep9979@gmail.com>
2020-07-12 18:19:23 +0530

    kmn.py file Added

--- abc.py
+++ abc.py
@@ -3,4 +3,5 @@ if(os.path.exists('input.txt')):
     sys.stdin=open('input.txt','r')
     sys.stdout=open('output.txt','w')
 for testcase in range(int(input())):
-    print("Hello World")    
+    print("Hello World")
+    
